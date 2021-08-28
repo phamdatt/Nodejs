40af1ad775f1a99b3de968018a921d6197134efc
Pham Dat <tiendatt3009@gmail.com>
2021-08-28 20:00:13 +0700

    add statis and boostrap

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const express = require('express') // su dung package express
 const morgan = require('morgan') //su dung package morgan ->xem request , hoac dung de debug
 const expressHandlebars = require('express-handlebars'); // su dung template
-const path =require('path') //lay duong dan 
+const path = require('path') //lay duong dan 
 const app = express()
 const port = 3000 // khoi tao port
 
+app.use(express.static(path.join(__dirname, 'public')));
 //Router
 //Logger
 app.use(morgan('combined'))
 //Template engine
 app.engine('hbs', expressHandlebars({
-    extname:'.hbs'
+    extname: '.hbs'
 }));
-app.set('view engine','hbs')
+app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resource/views')) // cau hinh duong dan cho views
 app.get('/', (request, response) => {
     response.render('home'); // di den file home
