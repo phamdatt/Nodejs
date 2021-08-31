b1594017ea7f4c6739308302a5101ad26025131f
Pham Dat <tiendatt3009@gmail.com>
2021-09-01 01:04:32 +0700

    deb-dat

--- src/app/controllers/SitesControllers.js
+++ src/app/controllers/SitesControllers.js
@@ -1,19 +1,19 @@
 const Products = require('../model/Products');
 class SitesControllers {
     index(request, response) {
-        // response.render('home');
+        response.render('home');
         // response.json({
         //     name: 'test'
         // });
-        Products.find({}, function (err, products) {
-            if (!err) {
-                response.json(products);
-            } else {
-                response.status(400).json({
-                    error: 'Error',
-                })
-            }
-        });
+        // Products.find({}, function (err, products) {
+        //     if (!err) {
+        //         response.json(products);
+        //     } else {
+        //         response.status(400).json({
+        //             error: 'Error',
+        //         })
+        //     }
+        // });
     }
 }
 module.exports = new SitesControllers;
