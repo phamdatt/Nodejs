2c871859fc193755205172c1f722a06b0f20f294
Pham Dat <tiendatt3009@gmail.com>
2021-09-13 01:07:30 +0700

    dev-dat

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
 const newsControllers = require('../app/controllers/NewsControllers')
-
+router.get('/', newsControllers.index);
+router.post('/', newsControllers.index);
 module.exports = router;
